03ae2745bb94986632e0ec331680b3c9d988224b
liborioc1 <liboriociccarello2@gmail.com>
2021-02-07 19:17:53 -0500

    #2 #15 #35 Handled uniqueness of username + users stored in database

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,26 +1,51 @@
 import React,{useCallback,useContext} from 'react';
 import {withRouter,Redirect} from "react-router";
-import {auth} from "../firebase"
+import {auth,database} from '../firebase';
 import SignUpBox from '../Components/SignUpBox';
 import {AuthenticationContext} from "../Authenticated";
 
 const SignUpPage =()=> {
     document.title ='Fakestagram - Sign Up';
-
     const handleSignUp = useCallback(async event => {
-
-    console.log("entered handleSignUp");    
+    
     event.preventDefault();
     const{username,email,password} = event.target.elements;
     console.log(username.value,email.value,password.value);
-        
+    var newUsername=true;
+
+    await database.collection('users').where('username','==',username.value).get('value').then((snapshot) =>{
     
-         await auth.createUserWithEmailAndPassword(email.value,password.value).then((authenticatedUser) =>  {
+            if(snapshot.empty){
+                newUsername=true;
+            }
+            else{
+                newUsername=false;
+                console.log(newUsername +"line 23");
+                throw new Error('Username is not unique: Try again!');
+            }
+
+        }).catch((error)=>alert(error.message));  
+        
+        if(newUsername===true){
+            console.log(newUsername+"line 30");
+        await auth.createUserWithEmailAndPassword(email.value,password.value).then((authenticatedUser) =>  {
+            database.collection('users').doc(authenticatedUser.user.uid).set({
+                followerCount:0,
+                following:[],
+                followingCount:0,
+                myPosts:[],
+                uid:authenticatedUser.user.uid,
+                username: username.value,
+            
+             });
+
             return authenticatedUser.user.updateProfile({
             displayName: username.value
             })
         }).catch((error)=>alert(error.message));
-        
+    }
+
+
     },[]);
 
     const{user} = useContext(AuthenticationContext);
